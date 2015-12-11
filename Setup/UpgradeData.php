b28c4382086064150cb8a45f8388a11fb0aa6c58
Michal Biarda <michal.biarda@orba.pl>
2015-12-11 15:01:15 +0100

    Re #223 - remove unnecessary upgrade script content

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -16,21 +16,6 @@ class UpgradeData implements UpgradeDataInterface
 {
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        $dbVersion = $context->getVersion();
-        $connection = $setup->getConnection();
 
-        if (version_compare($dbVersion, '0.1.0', '<')) {
-            // add Pending Payu.pl status to Pending Payment state
-            $connection->insert($setup->getTable('sales_order_status'), [
-                'status' => 'pending_payupl',
-                'label' => 'Pending Payu.pl'
-            ]);
-            $connection->insert($setup->getTable('sales_order_status_state'), [
-                'status' =>  'pending_payupl',
-                'state' => 'pending_payment',
-                'is_default' => 0,
-                'visible_on_front' => 1
-            ]);
-        }
     }
 }
